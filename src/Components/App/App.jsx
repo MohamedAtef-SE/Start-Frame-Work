58577eb8850cb6e181d7005f3e38cd0974f01ece
mohamedatef-SE <mohamedatefyoussef@gmail.com>
2024-02-05 21:25:36 +0200

    commited two

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -13,7 +13,7 @@ import Skills from '../Skills/Skills';
 
 const myBrowserRouter = createBrowserRouter([
   {
-    path: '/start-frame-work', element: <LayOut />, children: [
+    path: '/Start-Frame-Work', element: <LayOut />, children: [
       { index: true, element: <Home /> },
       { path: 'education', element: <Education /> },
       { path: 'courses', element: <Courses /> },
