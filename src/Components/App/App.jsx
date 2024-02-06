df7f8730c54f954c2728c4a364a4a905beffc29a
mohamedatef-SE <mohamedatefyoussef@gmail.com>
2024-02-06 09:48:01 +0200

    initial commit

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -11,9 +11,9 @@ import Skills from '../Skills/Skills';
 
 
 
-const myBrowserRouter = createBrowserRouter([
+const myBrowserRouter = createHashRouter([
   {
-    path: '/Start-Frame-Work', element: <LayOut />, children: [
+    path: '/', element: <LayOut />, children: [
       { index: true, element: <Home /> },
       { path: 'education', element: <Education /> },
       { path: 'courses', element: <Courses /> },
